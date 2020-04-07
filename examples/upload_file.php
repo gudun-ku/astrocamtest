ca5b18ef5969811d877addf85092f1f79442284e
Aleksandr Beloushkin <gudun.ku@gmail.com>
2020-04-07 13:52:50 +0400

    Changed file uploading method

--- examples/upload_file.php
+++ examples/upload_file.php
@@ -1,11 +1,13 @@
 <?php
+
 /**
  * This web service demonstrates how to handle uploaded files
  * in form of 'multipart/form-data'.
  *
  * @Route('upload')
  */
-class UploadService extends Service {
+class UploadService extends Service
+{
 
   /**
    * The upload method. We only accept png images for this demo.
@@ -16,7 +18,8 @@ class UploadService extends Service {
    *
    * @ContentType('image/png')
    */
-  public function post($file) {
+  public function post($file)
+  {
 
     // Check that we've got input
     if ($file !== NULL) {
@@ -26,17 +29,34 @@ class UploadService extends Service {
 
         // Return the image uploaded to demonstrate that we got it
         echo file_get_contents($file['tmp_name']);
+      } else if ($file['type'] == 'application/x-rar-compressed') {
 
+        // Return the image uploaded to demonstrate that we got it
+        $target_dir = "uploads/";
+        $path = pathinfo($file['name']);
+        $filename = $path['filename'];
+        $ext = $path['extension'];
+        $temp_name = $file['tmp_name'];
+        $path_filename_ext = $target_dir . $filename . "." . $ext;
+
+        if (file_exists($path_filename_ext)) {
+          echo "Sorry, file already exists.";
+        } else {
+          move_uploaded_file($temp_name, $path_filename_ext);
+          echo "Congratulations! File Uploaded Successfully.";
+        }
+        echo 'uploaded !' . $file;
       } else {
-        throw new ServiceException(HttpStatus::BAD_REQUEST,
-          'Uploaded file is not a png image');
+
+        throw new ServiceException(HttpStatus::BAD_GATEWAY, $file['type']);
       }
     } else {
-      throw new ServiceException(HttpStatus::BAD_REQUEST,
-        'Missing file');
+      throw new ServiceException(
+        HttpStatus::BAD_REQUEST,
+        'Missing file'
+      );
     }
   }
-
 }
 
 // The $server object is instantiated in index.php
